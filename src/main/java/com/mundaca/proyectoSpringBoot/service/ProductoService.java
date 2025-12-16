2052c76716eb9cede77fe457ab0c11deb5508f50
Cristóbal Mundaca <cristobal.mundacav@gmail.com>
2025-12-16 19:59:04 -0300

    Creacion de Controller y Realizacion de Pruebas Funcionando Correctamente

--- src/main/java/com/mundaca/proyectoSpringBoot/service/ProductoService.java
+++ src/main/java/com/mundaca/proyectoSpringBoot/service/ProductoService.java
@@ -1,5 +1,14 @@
 package com.mundaca.proyectoSpringBoot.service;
+import java.util.List;
+import java.util.Optional;
+import com.mundaca.proyectoSpringBoot.models.Producto;
+
 
 public interface ProductoService {
 
+    List<Producto> findAll();
+    Optional<Producto> findById(Long id);
+    Producto save(Producto producto);
+    Producto update(Long id,Producto producto);
+    void deleteById(Long id);
 }
